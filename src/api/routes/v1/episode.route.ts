d229f905a59a7f64b1f5c9fbeedf368ca8ae0ac9
zainulhassan815 <zainulhassan4330@gmail.com>
2022-08-12 20:20:07 +0500

    Remove dangerous post method.

--- src/api/routes/v1/episode.route.ts
+++ src/api/routes/v1/episode.route.ts
@@ -1,19 +1,8 @@
-import { RequestHandler, Router } from 'express';
+import { Router } from 'express';
 import * as EpisodesController from '../../controllers/episode.controller';
-import Episode from '../../models/Episode';
 
 const router = Router();
 
-const postHandler: RequestHandler = async (req, res, next) => {
-  try {
-    const data = req.body;
-    const response = await Episode.bulkCreate(data);
-    res.send(response);
-  } catch (e) {
-    next(e);
-  }
-};
-
-router.route('/').get(EpisodesController.getEpisodes).post(postHandler);
+router.route('/').get(EpisodesController.getEpisodes);
 
 export default router;
